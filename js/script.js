99404ed5c368f125d9bf6a107feab66a9c3b2ce3
Alexey <alexeyartberg@gmail.com>
2020-06-23 08:47:55 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -35,12 +35,12 @@ $( document ).ready(function() {
 
     //MODAL
     $('[data-modal=consultation]').on('click' , function(){
-        $('.overlay , #consultation').fadeIn();
+        $('.overlay , #modal-consultation').fadeIn();
     });
 
     $('[data-modal=order]').each(function(i){
       $(this).on('click' , function(){
-        $('.modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay , #order').fadeIn();
       });
     });
@@ -49,4 +49,36 @@ $( document ).ready(function() {
       $('.overlay , #consultation , #order , #thanks' ).fadeOut();
     });
 
+    function validateForms(form){
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: 
+          {
+            required: true,
+            email: true
+          }
+        },
+        messages : {
+          name: {
+            required: "Пожалуйства, введите имя",
+            minlength: jQuery.validator.format("Как минимум {0} символа!")
+          },
+          phone: "Пожалуйства, укажите ваш номер",
+          email: {
+            required: "Вы забыли указать почту",
+            email: "Используйте формат user@example.ua"
+          }
+        }
+        
+      });
+    }
+    validateForms('#consultation form')
+    validateForms('#modal-consultation form')
+    validateForms('#order form')
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
